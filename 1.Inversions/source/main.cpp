290b1b1aa752c0d631c953392742430ccbd96940
Shunji Lin <lgxshunji@gmail.com>
2016-10-29 16:07:38 +0900

    clean up 1.Inversions

--- 1.Inversions/source/main.cpp
+++ 1.Inversions/source/main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 #include "MergeSort.hpp"
 
 using namespace MergeSort;
@@ -9,10 +11,26 @@ using namespace MergeSort;
 int main(int argc, char *argv[]) {
     // Read from file into vector
     std::vector<int> testVector;
-    int entry;
-    while (std::cin >> entry) {
+
+    std::ifstream infile;
+    infile.open("../IntegerArray.txt");
+    if (!infile) {
+	std::cout << "Opening file failed." << std::endl;
+	return -1;
+    }
+
+    std::string line;
+    while (std::getline(infile, line)) {
+	std::istringstream iss(line);
+	int entry;
+	iss >> entry;
+	if (iss.fail()) {
+	    std::cout << "Reading error or incorrect type." << std::endl;
+	    return -1;
+	}
 	testVector.emplace_back(entry);
     }
+    
     unsigned inversions = 0;
     mergeSort(testVector, inversions);
     std::cout << "Number of inversions is : " << inversions << std::endl;
