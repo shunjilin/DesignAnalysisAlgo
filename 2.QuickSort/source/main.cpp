9198edb6144a8c6bea39003ddb49d65826796159
Shunji Lin <lgxshunji@gmail.com>
2016-10-29 16:39:21 +0900

    include timer

--- 2.QuickSort/source/main.cpp
+++ 2.QuickSort/source/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include "QuickSort.hpp"
+#include "steadyClockTimer.hpp"
 
 using namespace QuickSort;
 
@@ -34,6 +35,10 @@ int main(int argc, char *argv[]) {
     std::vector<int> testVector2(testVector1);
     std::vector<int> testVector3(testVector2);
 
+    
+    SteadyClockTimer timer;
+    timer.start();
+
     unsigned comparisons1 = 0;
     unsigned comparisons2 = 0;
     unsigned comparisons3 = 0;
@@ -50,4 +55,7 @@ int main(int argc, char *argv[]) {
     std::cout << "Number of comparisons is [first pivot] : " << comparisons1  << std::endl;
     std::cout << "Number of comparisons is [last pivot] : " << comparisons2  << std::endl;
     std::cout << "Number of comparisons is [median of three pivot] : " << comparisons3  << std::endl;
+
+     std::cout << "time taken for 3 sorts is: " << timer.getMs() << " ms" << std::endl;
+     return 0;
 }
